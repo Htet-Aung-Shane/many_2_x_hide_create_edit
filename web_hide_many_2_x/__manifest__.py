2e74f17f7d71eca11a1fd72193aae98ead21a577
Htet-Aung-Shane <mmc.htetaungshane@gmail.com>
2024-09-02 12:56:36 +0630

    update manifest

--- web_hide_many_2_x/__manifest__.py
+++ web_hide_many_2_x/__manifest__.py
@@ -1,15 +1,29 @@
 {
     "name": "Web Hide Many2 X Option",
-    "author":"MT Tech",
-    'application':True,
-    'installable': True,
-    'license':'LGPL-3',
-    'version': '17.0.0.0',
-    'depends': ['web', 'website_enterprise'],
-    'assets': {
-        'web.assets_backend': [
-            'web_hide_many_2_x/static/src/js/many_2_one.js',
-            'web_hide_many_2_x/static/src/js/many_2_many.js',
+    "description": """
+        <h2>Web Hide Many2 X Option</h2>
+        <p>This module hides the Create and Edit options in Many2One and Many2Many fields in Odoo.</p>
+        <ul>
+            <li>Author: MT Tech</li>
+            <li>Version: 17.0.0.0</li>
+            <li>License: LGPL-3</li>
+        </ul>
+        <p>Features:</p>
+        <ul>
+            <li>Removes Create and Edit options from selection fields</li>
+            <li>Works with both backend and frontend views</li>
+        </ul>
+    """,
+    "author": "MT Tech",
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+    "version": "17.0.0.0",
+    "depends": ["web", "website_enterprise"],
+    "assets": {
+        "web.assets_backend": [
+            "web_hide_many_2_x/static/src/js/many_2_one.js",
+            "web_hide_many_2_x/static/src/js/many_2_many.js",
         ],
     },
 }
