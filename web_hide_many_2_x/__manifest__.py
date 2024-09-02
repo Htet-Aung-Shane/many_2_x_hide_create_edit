d27e359fa4af8bb0f17867838d04c1632416cedc
Htet-Aung-Shane <mmc.htetaungshane@gmail.com>
2024-09-02 14:08:43 +0630

    update manifest

--- web_hide_many_2_x/__manifest__.py
+++ web_hide_many_2_x/__manifest__.py
@@ -1,18 +1,6 @@
 {
     "name": "Web Hide Many2 X Option",
-    "description": """
-        <h2>Web Hide Many2 X Option</h2>
-        <p>This module hides the Create and Edit options in Many2One and Many2Many fields in Odoo.</p>
-        <ul>
-            <li>Author: MT Tech</li>
-            <li>Version: 17.0.0.0</li>
-            <li>License: LGPL-3</li>
-        </ul>
-        <p>Features:</p>
-        <ul>
-            <li>Removes Create and Edit options from selection fields</li>
-            <li>Works with both backend and frontend views</li>
-        </ul>
+    "description": """This module hides the Create and Edit options in Many2One and Many2Many fields in Odoo.
     """,
     "author": "MT Tech",
     "application": True,
